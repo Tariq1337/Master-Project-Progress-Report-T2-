bff3788688fc72b364eaff18493b2247f679e9d9
mmakei <115322276+mmakei@users.noreply.github.com>
2022-10-07 13:57:19 -0700

    Create Page 3.js

---
+++ Page 3.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(300, 500);
+  noStroke();
+}
+
+function draw() {
+  background(220);
+  fill('#A8DDFC');
+    rect(0, 0, 1200, 110);
+  textSize(30)
+  fill(50);
+  text('Game Name', 65, 65);
+}
